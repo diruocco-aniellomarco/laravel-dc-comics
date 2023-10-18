1a6483d5bcc48b5ec8fe10f09279289b1483471f
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-10-18 15:59:57 +0200

    milestone 1

---
+++ database/migrations/2023_10_18_121959_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('description',200);
+            $table->string('thumb', 200);
+            $table->string('price', 8);
+            $table->string('series', 20);
+            $table->date('sale_date');
+            $table->string('type', 30);
+                       
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('main-content')
+  <section class="container mt-5">
+    
+
+    <table class="table table-striped">
+        <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">title</th>
+      <th scope="col">description</th>
+      <th scope="col">Link img</th>
+      <th scope="col">price</th>
+      <th scope="col">series</th>
+      <th scope="col">Sale date</th>
+      <th scope="col">type</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($comics as $comic)
+        <tr>
+        <th scope="row">{{$comic->id}}</th>
+        <td>{{$comic->title}}</td>
+        <td>{{$comic->description}}</td>
+        <td>{{$comic->thumb}}</td>
+        <td>{{$comic->price}}</td>
+        <td>{{$comic->series}}</td>
+        <td>{{$comic->sale_date}}</td>
+        <td>{{$comic->type}}</td>
+        </tr>
+    @endforeach
+  </tbody>
+    
+    </table>
+        
+   
+  </section>
+@endsection
